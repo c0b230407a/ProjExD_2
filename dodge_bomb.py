44d30e10d83ee786d49776c24c15b68d9cc7e006
R_k <c0b230407a@edu.teu.ac.jp>
2024-04-23 16:58:54 +0900

    追加課題3　完了

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import pygame as pg
+import time
 import random
 
 WIDTH, HEIGHT = 1600, 900
@@ -22,16 +23,46 @@ def bound (obj_rct)-> tuple[bool,bool]:
 
 
 
+def Game_Over(Brackout,Brackout_rct,txt):
+    screen = pg.display.set_mode((WIDTH, HEIGHT))
+    font = pg.font.Font(None,80)
+    txt=font.render("GAME OVER",True,(255,255,255))
+    Brackout_rct=WIDTH,HEIGHT
+    Brackout=pg.draw.rect(Brackout,(255,255,255),(WIDTH,HEIGHT))
+    clock=pg.time.Clock()
+    screen.blit(Brackout,Brackout_rct)
+    pg.time.wait(100)
+    pg.display.update()
+    pg.time.wait(100)
+    screen.blit(txt,[WIDTH/2,HEIGHT/2])
+    pg.display.update()
+
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("fig/pg_bg.jpg")    
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 2.0)
-    kk_rct = kk_img.get_rect()
+    kk_img2=pg.transform.rotozoom(pg.image.load("fig/8.png"), 0, 2.0)    #こうかとんの画像の取得
+    kk_rct = kk_img.get_rect()         
+    kk_rct2=kk_img2.get_rect()        #こうかとんの位置の取得
     kk_rct.center = 900, 400
     clock = pg.time.Clock()
     tmr = 0
     
+    accs = [a for a in range(1,11)]
+
+
+#ブラックアウトの設定
+    Brackout_img=pg.Surface((WIDTH,HEIGHT))    
+    font = pg.font.Font(None,80)
+    Brackout_img.set_alpha(160)  #半透明
+    Brackout_rct=Brackout_img.get_rect()
+    Brackout_rct.center=WIDTH/2,HEIGHT/2   #位置の設定
+    txt=font.render("GAME OVER",True,(255,255,255))    #文字の設定
+    Brackout=pg.draw.rect(Brackout_img,(0,0,0),(0,0,WIDTH,HEIGHT))
+
+
     bom_img= pg.Surface((20,20))
     bom_rct=bom_img.get_rect()
     bom_img.set_colorkey((0,0,0))
@@ -44,9 +75,16 @@ def main():
             if event.type == pg.QUIT: 
                 return
             
-        if kk_rct.colliderect(bom_rct) ==True:
-            print("Game Over")
-            return
+        if kk_rct.colliderect(bom_rct) ==True:    #こうかとんと爆弾が当たったとき
+            screen.blit(Brackout_img,Brackout_rct)
+            screen.blit(txt,[WIDTH/2 - 170, HEIGHT/2 - 40])
+            pg.display.update()
+            screen.blit(kk_img2, kk_rct2)
+            pg.display.update()
+            pg.time.wait(5000)  
+            return 
+
+    
         screen.blit(bg_img, [0, 0]) 
 
         key_lst = pg.key.get_pressed()
@@ -56,6 +94,11 @@ def main():
                 sum_mv[0]+=v[0]
                 sum_mv[1]+=v[1]
         kk_rct.move_ip(sum_mv)
+
+
+
+
+
         if bound(kk_rct) !=(True,True):
             kk_rct.move_ip(-sum_mv[0],-sum_mv[1])
 
